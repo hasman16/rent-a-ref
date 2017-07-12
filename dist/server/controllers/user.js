e03c0e20b14ef0039af35e521c62b54474c02ce8
Michael Godden <smylydon@yahoo.com>
2017-07-11 19:38:23 -0700

    To Heroku

--- dist/server/controllers/user.js
+++ dist/server/controllers/user.js
@@ -11,27 +11,72 @@ var __extends = (this && this.__extends) || (function () {
 })();
 Object.defineProperty(exports, "__esModule", { value: true });
 var jwt = require("jsonwebtoken");
-var user_1 = require("../models/user");
 var base_1 = require("./base");
+var bcrypt = require("bcryptjs");
+function hash(value) {
+    return bcrypt.hash(value, 10);
+}
+;
 var UserCtrl = (function (_super) {
     __extends(UserCtrl, _super);
-    function UserCtrl() {
-        var _this = _super !== null && _super.apply(this, arguments) || this;
-        _this.model = user_1.default;
+    function UserCtrl(User) {
+        var _this = _super.call(this) || this;
+        _this.model = null;
         _this.login = function (req, res) {
-            _this.model.findOne({ email: req.body.email }, function (err, user) {
-                if (!user) {
-                    return res.sendStatus(403);
+            var user = {
+                email: req.body.email,
+                password: req.body.password
+            };
+            function testing() {
+                console.log('testing');
+            }
+            function authorizationFailed() {
+                res.status(403).json({
+                    success: false,
+                    message: 'Authorization failed'
+                });
+            }
+            _this.model.findOne({
+                where: { email: user.email }
+            }).then(function (newUser) {
+                var token = null;
+                if (newUser) {
+                    console.log('hash:', user.password, newUser.password);
+                    return bcrypt.compare(user.password, newUser.password)
+                        .then(function (result) {
+                        console.log('result is:', result);
+                        if (result) {
+                            token = jwt.sign(user, process.env.SECRET_TOKEN, {
+                                expiresIn: 1440 * 60
+                            });
+                            res.status(200).json({
+                                success: true,
+                                message: 'Authorization success',
+                                token: token,
+                                accessLevel: newUser.authorization
+                            });
+                        }
+                        else {
+                            authorizationFailed();
+                        }
+                    });
                 }
-                user.comparePassword(req.body.password, function (error, isMatch) {
-                    if (!isMatch) {
-                        return res.sendStatus(403);
-                    }
-                    var token = jwt.sign({ user: user }, process.env.SECRET_TOKEN); // , { expiresIn: 10 } seconds
-                    res.status(200).json({ token: token });
+                else {
+                    authorizationFailed();
+                }
+            })
+                .catch(function (error) {
+                console.log('error:', error);
+                res.status(500).json({
+                    success: false,
+                    message: 'Error occurred'
                 });
             });
         };
+        _this.logout = function (req, res) {
+            var user = new Object(req.body);
+        };
+        _this.model = User;
         return _this;
     }
     return UserCtrl;
