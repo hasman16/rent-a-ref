697db4a9275962694542f065c38e49fba64f7f8a
Ahmadou <hasman16@gmail.com>
2017-09-04 17:00:56 -0700

    Fetching Mike update

--- client/app/account/profile/deactivated/deactivated.component.ts
+++ client/app/account/profile/deactivated/deactivated.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../services/user.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-deactivated',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./deactivated.component.scss']
 })
 export class DeactivatedComponent implements OnInit {
+  user = { id: '', email: '' };
+  person = { id: '' };
 
-  constructor() { }
+  constructor(private auth: AuthService, private userService: UserService) { }
 
   ngOnInit() {
+    this.getProfile();
   }
+  getProfile() {
+    this.userService.getProfile(this.auth.currentUser.id).subscribe(
+      res => {
+        this.user = res;
 
+        this.person = res.person;
+        // this.isLoading = false;
+      },
+
+      (err: HttpErrorResponse) => {
+        if (err.error instanceof Error) {
+          // A client-side or network error occurred. Handle it accordingly.
+          console.log('A client-side or network error occurred for the Profile');
+        } else {
+          console.log('The backend returned an unsuccessful response code for the profile');
+
+        }
+        // this.isLoading = false;
+        if (!this.auth.loggedIn) {
+          // this.abort = true;
+          this.auth.logout();
+        }
+      }
+    );
+  }
 }
