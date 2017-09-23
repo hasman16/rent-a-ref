cd63ea5e20c73d4a0ad48cb9fe12ca259b568a50
Michael Godden <smylydon@yahoo.com>
2017-09-22 18:36:37 -0700

    #25 continuing to refactor profile form.

--- client/app/account/profile/edit-profile/address-form/address-form.component.ts
+++ client/app/account/profile/edit-profile/address-form/address-form.component.ts
@@ -1,7 +1,9 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, AbstractControl, Validators, FormBuilder } from '@angular/forms';
 
 import { AddressType } from '../../../../shared/models/addressType';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/debounceTime';
 
 @Component({
   selector: 'address-form',
@@ -15,8 +17,11 @@ export class AddressFormComponent implements OnInit {
 
   addressForm: FormGroup;
   alphaNumericRegex: '[a-zA-Z0-9_-\\s]*';
-  zipRegex: '^\\d{5}(?:[ -]{1}\\d{4})?$';
+  zipRegex: '\\d{5}|\\d{5}((\\s|-)\\d{4})';
   showDivAddress = true;
+  line1Invalid = false;
+  cityInvalid = false;
+  zipInvalid = false;
 
   constructor(private formBuilder: FormBuilder) {
     this.addressForm = this.formBuilder.group({
@@ -28,12 +33,31 @@ export class AddressFormComponent implements OnInit {
       city: ['', [Validators.required, Validators.minLength(2),
       Validators.maxLength(30), Validators.pattern(this.alphaNumericRegex)]],
 
-      state: ['', [Validators.required, Validators.minLength(2),
-      Validators.maxLength(20), Validators.pattern(this.alphaNumericRegex)]],
+      state: ['', [Validators.required, Validators.maxLength(10), Validators.pattern(this.alphaNumericRegex)]],
 
-      zip: ['', [Validators.required, Validators.minLength(2),
-      Validators.maxLength(12), Validators.pattern(this.zipRegex)]]
+      zip: ['', [Validators.required, Validators.minLength(5),
+      Validators.maxLength(10), Validators.pattern(this.zipRegex)]]
     });
+    let line1 = this.addressForm.get('line1');
+    let city = this.addressForm.get('city');
+    let zip = this.addressForm.get('zip');
+
+    this.validator(line1, 'line1Invalid');
+    this.validator(city, 'cityInvalid');
+    this.validator(zip, 'zipInvalid');
+  }
+
+  validator(item:AbstractControl , name: string) {
+    item
+      .valueChanges
+      .debounceTime(1000)
+      .subscribe((value) => {
+        let result = false;
+        if (item.touched && item.invalid) {
+          result = true;
+        }
+        this[name] = result;
+      });
   }
 
   ngOnInit() {
