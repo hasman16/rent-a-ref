441b1ff598b3a48df9e67f5de6eaf403f6efa736
Michael Godden <smylydon@yahoo.com>
2017-10-03 13:51:36 -0700

    #25 Finally figured out how to set up unit testing for reactive forms.

--- client/app/account/profile/edit-profile/address-form/address-form.component.spec.ts
+++ client/app/account/profile/edit-profile/address-form/address-form.component.spec.ts
@@ -5,77 +5,138 @@ import { FormGroup, FormControl, AbstractControl, Validators, FormBuilder, React
 import { AddressType } from '../../../../shared/models/addressType';
 
 import { AddressFormComponent } from './address-form.component';
+import { ProfileService } from '../../../../services/profile.service';
+import { StatesService } from '../../../../services/states.service';
 
-xdescribe('AddressFormComponent', () => {
+let profileServiceStub = {
+  createAddress: function() { },
+  updateAddress: function() { }
+};
+
+let statesServiceStub = {
+  getStatesProvinces: function() {
+    return [{
+      "name": "Arkansas",
+      "abbreviation": "AR"
+    },
+    {
+      "name": "California",
+      "abbreviation": "CA"
+    },
+    {
+      "name": "Colorado",
+      "abbreviation": "CO"
+    }];
+  }
+};
+
+describe('AddressFormComponent', () => {
   let component: AddressFormComponent;
   let fixture: ComponentFixture<AddressFormComponent>;
+  let addressForm;
+  let line1;
+  let city;
+  let zip;
+  let state;
 
-  beforeEach(async(() => {
+  let setUp = () => {
     TestBed.configureTestingModule({
-      declarations: [ AddressFormComponent ],
-      providers: [ FormBuilder ]
-    })
-    .compileComponents()
-    .then(() => {
-      fixture = TestBed.createComponent(AddressFormComponent);
-      component = fixture.componentInstance;
-    })
-  }));
+      imports: [ReactiveFormsModule],
+      declarations: [AddressFormComponent],
+      providers: [{ provide: ProfileService, useValue: profileServiceStub }, { provide: StatesService, useValue: statesServiceStub }]
+    });
+  };
+  let createCompoent = () => {
+    fixture = TestBed.createComponent(AddressFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  let getValues = () => {
+    addressForm = component.addressForm;
+    line1 = addressForm.get('line1');
+    city = addressForm.get('city');
+    zip = addressForm.get('zip');
+    state = addressForm.get('state');
+  }
+
+  describe("Address Line 1", () => {
+    beforeEach(setUp);
+
+    beforeEach(createCompoent);
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('should be invalid and empty string.', () => {
+      getValues();
+      const de = fixture.debugElement.query(By.css('div'));
+      expect(de).toBeNull();
+      expect(line1.invalid).toBeTruthy();
+      expect(line1.getValue()).toBe('');
+    });
+
+    it('should be valid and a string.', () => {
+      getValues();
+      line1.setValue('24 Main St.');
+      fixture.detectChanges();
+      const de = fixture.debugElement.query(By.css('[formControlName="line1"]'));
+      const el = de.nativeElement;
+      expect(de).toBeDefined();
+      expect(line1.invalid).toBeTruthy();
+      expect(line1.getValue()).toBe('');
+    });
   });
 
-  it('should be invalid', () => {
-    let addressForm = component.addressForm;
-    let line1 = addressForm.get('line1');
-    let city = addressForm.get('city');
-    let zip = addressForm.get('zip');
-    let state = addressForm.get('state');
+  describe("AddressForm", () => {
+    beforeEach(setUp);
 
-    expect(addressForm.invalid).toBeTruthy();
-    expect(line1.invalid).toBeTruthy();
-    expect(city.invalid).toBeTruthy();
-    expect(state.invalid).toBeTruthy();
-    expect(zip.invalid).toBeTruthy();
-  });
+    beforeEach(createCompoent);
 
-  it('should be invalid due bad street address', () => {
-    let addressForm = component.addressForm;
-    let line1 = addressForm.get('line1');
-    let city = addressForm.get('city');
-    let zip = addressForm.get('zip');
-    let state = addressForm.get('state');
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
 
-    line1.setValue('');
-    city.setValue('Orange');
-    zip.setValue('92111');
-    state.setValue('CA');
+    it('should be invalid', () => {
+      getValues();
 
-    expect(addressForm.invalid).toBeTruthy();
-    expect(line1.invalid).toBeTruthy();
-    expect(city.valid).toBeTruthy();
-    expect(state.valid).toBeTruthy();
-    expect(zip.valid).toBeTruthy();
-  });
+      expect(addressForm.invalid).toBeTruthy();
+      expect(line1.invalid).toBeTruthy();
+      expect(city.invalid).toBeTruthy();
+      expect(state.invalid).toBeTruthy();
+      expect(zip.invalid).toBeTruthy();
+    });
 
-  it('should be valid', () => {
-    let addressForm = component.addressForm;
-    let line1 = addressForm.get('line1');
-    let city = addressForm.get('city');
-    let zip = addressForm.get('zip');
-    let state = addressForm.get('state');
+    it('should be invalid due to bad street address', () => {
+      getValues();
 
-    line1.setValue('42 Main St');
-    city.setValue('Orange');
-    zip.setValue('92111');
-    state.setValue('CA');
+      line1.setValue('');
+      city.setValue('Orange');
+      zip.setValue('92111');
+      state.setValue('CA');
 
-    expect(addressForm.valid).toBeTruthy();
-    expect(line1.valid).toBeTruthy();
-    expect(city.valid).toBeTruthy();
-    expect(state.valid).toBeTruthy();
-    expect(zip.valid).toBeTruthy();
+      expect(addressForm.invalid).toBeTruthy();
+      expect(line1.invalid).toBeTruthy();
+      expect(city.valid).toBeTruthy();
+      expect(state.valid).toBeTruthy();
+      expect(zip.valid).toBeTruthy();
+    });
+
+    it('should be valid', () => {
+      getValues();
+
+      line1.setValue('42 Main St');
+      city.setValue('Orange');
+      zip.setValue('92111');
+      state.setValue('CA');
+
+      expect(addressForm.valid).toBeTruthy();
+      expect(line1.valid).toBeTruthy();
+      expect(city.valid).toBeTruthy();
+      expect(state.valid).toBeTruthy();
+      expect(zip.valid).toBeTruthy();
+    });
   });
 
 });
