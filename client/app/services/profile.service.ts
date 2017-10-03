7d581783cf67cc8ffe9bc915ffbc1f5d03048d55
Michael Godden <smylydon@yahoo.com>
2017-10-02 22:32:20 -0700

    #25 fixed bug in server code, user was able to save address with id = 0. More refactoring.

--- client/app/services/profile.service.ts
+++ client/app/services/profile.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { UserService } from './user.service';
-import _ from "lodash";
+
 import { AddressType } from "./../shared/models/addressType";
 import { BioType } from "./../shared/models/bioType";
 import { PhoneType } from "./../shared/models/phoneType";
+import { Observable } from 'rxjs/Observable';
+import _ from "lodash";
 
 @Injectable()
 export class ProfileService {
-  private data; //  { id: '', email: '', person: [], addresses: []};
-  private person; // = { id: ''};
+  private data;
+  private person;
   private addresses = [];
   private areas = [];
   private phones = [];
@@ -18,7 +20,7 @@ export class ProfileService {
   }
 
   getData() {
-    return _.clone(this.data);
+    return _.cloneDeep(this.data);
   }
 
   getPerson():BioType {
@@ -47,5 +49,21 @@ export class ProfileService {
         this.areas = res.areas;
         return res;
       });
+  }
+
+  createAddress(newAddress: AddressType):Observable<any> {
+    return this.userService.createAddress(newAddress, this.data.id);
+  }
+
+  updateAddress(newAddress: AddressType):Observable<any> {
+    return this.userService.updateAddress(newAddress, this.data.id, newAddress.id);
+  }
+
+  createPhone(newPhone: PhoneType):Observable<any> {
+    return this.userService.createPhone(newPhone, this.data.id);
+  }
+
+  updatePhone(newPhone: PhoneType):Observable<any> {
+    return this.userService.updatePhone(newPhone, this.data.id, newPhone.id);
   }
 }
