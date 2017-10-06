1295eb62b3c460eec2c0a6e0ed22c369f2c7bf59
Michael Godden <smylydon@yahoo.com>
2017-10-06 13:15:51 -0700

    GOING TO HERUKU

---
+++ dist/server/models/Lock.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function default_1(sequelize, DataTypes) {
+    return sequelize.define('lock', {
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        attempts: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        passcode: {
+            type: DataTypes.STRING
+        },
+        passcode_age: {
+            type: DataTypes.DATE
+        }
+    }, {
+        paranoid: true,
+        underscored: true // use underscore instead of camelCase.
+    });
+}
+exports.default = default_1;
+//# sourceMappingURL=Lock.js.map
