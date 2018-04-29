123afc859079964ec84b73102aa73a5aefd104a4
Michael Godden <smylydon@yahoo.com>
2018-04-29 13:43:37 -0700

    GOING TO HEROKU.

---
+++ dist/server/routes/personRoutes.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function personRoutes(setter, personCtrl) {
+    var router = setter.router;
+    var authentication = setter.authentication;
+    var authorization = setter.authorization;
+    var uploads = setter.uploads;
+    var isUserOrAdmin = authorization.isUserOrAdmin;
+    var isAdmin = authorization.isAdmin;
+    router.route('/people').get(authentication, isAdmin, personCtrl.getAll);
+    router.route('/people/:person_id').get(authentication, personCtrl.getOne);
+    router.route('/people/:person_id').put(authentication, personCtrl.update);
+    router.route('/people/:person_id').patch(authentication, personCtrl.update);
+    router.route('/people/:person_id').delete(authentication, isAdmin, personCtrl.deleteOne);
+}
+exports.default = personRoutes;
+//# sourceMappingURL=personRoutes.js.map
