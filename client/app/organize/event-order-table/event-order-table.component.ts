abe34200a16019507973088f65b846851a6daf74
Michael Godden <smylydon@yahoo.com>
2018-05-14 22:20:36 -0700

    GOING TO HEROKU.

--- client/app/organize/event-order-table/event-order-table.component.ts
+++ client/app/organize/event-order-table/event-order-table.component.ts
@@ -20,20 +20,34 @@ export class EventOrderTableComponent implements OnInit {
     this.model = Object.assign(
       {},
       {
-        kids_ref_total: model.kids_ref_pay * model.kids_referees,
-        teens_ref_total: model.teens_ref_pay * model.teens_referees,
-        adults_ref_total: model.adults_ref_pay * model.adults_referees
+        kids_ref_total: 0,
+        teens_ref_total: 0,
+        adults_ref_total: 0
       },
       model
     );
-    this.model.total =
-      this.model.kids_ref_total +
-      this.model.teens_ref_total +
-      this.model.adults_ref_total;
-    this.cd.markForCheck();
+    this.update();
   }
+
+  public inputKidsRefpay: boolean = false;
+  public inputTeensRefpay: boolean = false;
+  public inputAdultRefpay: boolean = false;
   public model: any;
   constructor(private cd: ChangeDetectorRef) {}
 
   ngOnInit() {}
+
+  update(): void {
+    const model = this.model;
+    this.inputAdultRefpay = false;
+    this.inputKidsRefpay = false;
+    this.inputTeensRefpay = false;
+    (model.kids_ref_total = model.kids_ref_pay * model.kids_referees),
+      (model.teens_ref_total = model.teens_ref_pay * model.teens_referees),
+      (model.adults_ref_total = model.adults_ref_pay * model.adults_referees);
+
+    model.total =
+      model.kids_ref_total + model.teens_ref_total + model.adults_ref_total;
+    this.cd.markForCheck();
+  }
 }
