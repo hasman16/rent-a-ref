561dda4930def729de592d37a82878b52762a13a
Michael Godden <smylydon@yahoo.com>
2018-11-04 12:42:17 -0800

    GOING TO HEROKU.

---
+++ dist/server/models/Officiating.js
@@ -0,0 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Officiating = function (sequelize, DataTypes) {
+    return sequelize.define('officiating', {
+        position: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                isIn: [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]]
+            }
+        },
+        status: {
+            type: DataTypes.STRING(32),
+            allowNull: false,
+            defaultValue: 'pending',
+            validate: {
+                isIn: [['pending', 'accepted', 'declined', 'cancelled']]
+            }
+        }
+    }, {
+        paranoid: true,
+        underscored: true // use underscore instead of camelCase.
+    });
+};
+exports.default = Officiating;
+//# sourceMappingURL=Officiating.js.map
---
+++ dist/server/routes/matchRoutes.js
@@ -0,0 +1,45 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function matchRoutes(setter, matchCtrl) {
+    var router = setter.router;
+    var authentication = setter.authentication;
+    var authorization = setter.authorization;
+    var uploads = setter.uploads;
+    var isUserOrAdmin = authorization.isUserOrAdmin;
+    var isAdmin = authorization.isAdmin;
+    var isOrgOwner = authorization.isOrgOwner;
+    var isOrgMember = authorization.isOrgMember;
+    router.route('/matches').get(authentication, matchCtrl.getAll);
+    router.route('/matches').post(authentication, matchCtrl.create);
+    router.route('/matches/:match_id').get(authentication, matchCtrl.getOne);
+    router
+        .route('/matches/:match_id')
+        .put(authentication, isAdmin, matchCtrl.update);
+    router
+        .route('/matches/:match_id')
+        .patch(authentication, isAdmin, matchCtrl.update);
+    router
+        .route('/matches/:match_id')
+        .delete(authentication, isAdmin, matchCtrl.deleteOne);
+    router
+        .route('/matches/:match_id/address')
+        .get(authentication, matchCtrl.getMatchAddress);
+    router
+        .route('/meeting/:meeting_id/matches')
+        .get(authentication, matchCtrl.getAllByMeeting);
+    router
+        .route('/meeting/:meeting_id/matches')
+        .post(authentication, matchCtrl.createMatchAddressPhone);
+    router.use('/matches/:match_id/address/:address_id', authentication, isOrgMember, isUserOrAdmin);
+    router
+        .route('/matches/:match_id/address/:address_id')
+        .put(matchCtrl.updateMatchAddress);
+    router
+        .route('/matches/:match_id/address/:address_id')
+        .patch(matchCtrl.updateMatchAddress);
+    router
+        .route('/matches/:match_id/address/:address_id')
+        .delete(matchCtrl.deleteMatchAddress);
+}
+exports.default = matchRoutes;
+//# sourceMappingURL=matchRoutes.js.map
